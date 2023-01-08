acf7bbd0e1470f890fce915acd874e1c238c50c7
Alexey Fayzullin <alexey@fayzullin.com>
2023-01-08 23:21:34 +0700

    chore: add seting of debug log level via DEBUG=true

--- internal/pkg/log/log.go
+++ internal/pkg/log/log.go
@@ -1,10 +1,18 @@
 package log
 
 import (
+	"os"
+	"strings"
+
 	"go.uber.org/zap"
 )
 
 var (
-	// Logger = zap.Must(zap.NewProduction()).Sugar()
-	Logger = zap.Must(zap.NewDevelopment()).Sugar()
+	Logger = zap.Must(zap.NewProduction()).Sugar()
 )
+
+func init() {
+	if strings.ToLower(os.Getenv("DEBUG")) == "true" {
+		Logger = zap.Must(zap.NewDevelopment()).Sugar()
+	}
+}
